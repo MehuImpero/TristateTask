9e197a17dc056c16cccbdee2e1b4a31c31a16560
MehuImpero <mehul.imperoit@gmail.com>
2021-01-14 15:59:59 +0530

    React Native Practical By Mehul

--- src/component/Api.js
+++ src/component/Api.js
@@ -2,28 +2,9 @@ var MainURL = "https://evermed.in/";
 
 
 class ApiFetch {
-    PostData = (apiurl, myObj, method) => {
-      var API_URL = MainURL + apiurl;
-        console.log("API ---> " + API_URL)
-        console.log("OBJ ---> " + JSON.stringify(myObj))
-        return fetch(API_URL, {
-            method: method,
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body:myObj,
-        })
-            .then((response) => response.json())
-            .then((responseJson) => {
-              
-              //  console.log(JSON.stringify(responseJson))
-                return responseJson;
-            }).catch((error) => {
-                console.log(error.responseJson)
-                return error;
-            });
-    }
+
+ 
+
     PostDataWithToken = (apiurl, myObj, method, Token) => {
         var API_URL = MainURL + apiurl;
         console.log("API ---> " + API_URL)
@@ -47,6 +28,7 @@ class ApiFetch {
                 return error;
             });
     }
+
     PostDataForToken = (apiurl, method, Token) => {
         var API_URL = MainURL + apiurl;
         console.log("API ---> " + API_URL)
@@ -66,49 +48,6 @@ class ApiFetch {
             }).catch((error) => {
                 console.log(error.responseJson)
                 return error;
-            });
-    }
-
-
-    PostDataWithHeaderWithMultipart = (apiurl, myObj, method, Bearer) => {
-    var API_URL = MainURL + apiurl;
-        console.log("API ---> " + API_URL)
-        console.log("OBJ ---> " + JSON.stringify(myObj))
-        return fetch(API_URL, {
-            method: method,
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'multipart/form-data',
-            },
-            body:myObj,
-        })
-            .then((response) => response.json())
-            .then((responseJson) => {
-               // console.log(responseJson)
-                return responseJson;
-            }).catch((error) => {
-                console.log(error.responseJson)
-                return error;
-            });
-    }
-    
-    GetDataWithHeader = (apiurl, Bearer) => {
-        var API_URL = MainURL + apiurl;
-        console.log("API ---> " + API_URL)
-        return fetch(API_URL, {
-            method: "GET",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'multipart/form-data',
-            },
-        })
-            .then((response) => response.json())
-            .then((responseJson) => {
-               // console.log(responseJson)
-                return responseJson;
-            }).catch((error) => {
-                console.log(error.responseJson)
-                return error.responseJson;
             });
     }
 }
